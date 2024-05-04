90f8e8b9575668c9f0842f15c631eb3f99b9fc9b
Muhammad Danish <Muhammad.danish1015@gmail.com>
2024-05-04 15:25:44 +0500

    Update app.js

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ var p1Button = document.getElementById("p1");
 var p2Button = document.getElementById("p2");
 var p1Display = document.getElementById("p1Display");
 var p2Display = document.getElementById("p2Display");
-var resetButton = document.getElementById("reset")
-var numInput = document.getElementById("numDisplay")
-var winningScoreDisplay = document.getElementById("numChange")
+var resetButton = document.getElementById("reset");
+var numInput = document.getElementById("numDisplay");
+var winningScoreDisplay = document.getElementById("numChange");
 var p1Score = 0;
 var p2Score = 0;
 var gameOver = false;
@@ -30,18 +30,22 @@ p2Button.addEventListener("click", function () {
     p2Display.textContent = p2Score;
   }
 });
-resetButton.addEventListener("click", function(){
+resetButton.addEventListener("click", function () {
+  reset();
+});
+
+function reset(){
   p1Score = 0;
   p2Score = 0;
   p1Display.textContent = 0;
   p2Display.textContent = 0;
-  p1Display.style.color = "white"
-  p2Display.style.color = "white"
+  p1Display.style.color = "white";
+  p2Display.style.color = "white";
+  gameOver = false;
 
-  gameOver = false
-
-})
-numInput.addEventListener("change", function(){
-  winningScoreDisplay.textContent = numInput.value
-  winningScore = numInput.value
-})
+}
+numInput.addEventListener("change", function () {
+  winningScoreDisplay.textContent = numInput.value;
+  winningScore = Number(numInput.value);
+  reset();
+});
